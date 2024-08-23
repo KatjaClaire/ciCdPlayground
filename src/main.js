aff9db72dc0f80094db2417de4dff48bad8b17a0
KatjaClaire <51196284+KatjaClaire@users.noreply.github.com>
2024-08-23 08:02:23 +0200

    fill in personal infos

--- src/main.js
+++ src/main.js
@@ -4,25 +4,26 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Katja Koerner',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'(automated) pipeline for robust integration of changes into deployable code'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: 'GitHub Actions',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'more about Jenkins and the basics of CI/CD'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'painting', 
+			'crafting',
+			'reading books',
+			'music'
 		]
 	}
 });
